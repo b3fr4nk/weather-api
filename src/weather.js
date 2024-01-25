bb318d37b0f3443b8d10289e4e5a4a7df0b91b47
Chris Wood <wchristopherwood912@gmail.com>
2024-01-25 15:23:42 -0800

    add wind speed

--- src/weather.js
+++ src/weather.js
@@ -21,12 +21,15 @@ function Weather(props) {
   if (weather === "error" || weather["main"] === undefined) {
     return <h1>There was an error when trying to get the weather</h1>;
   }
+  console.log(weather);
 
   return (
     <div className="Weather">
       <h1>
         The temperature in {city} is {weather["main"]["temp"]}{" "}
-        {units === "imperial" ? "f" : units === "metric" ? "c" : "k"}
+        {units === "imperial" ? "f" : units === "metric" ? "c" : "k"} and the
+        wind speed is {weather["wind"]["speed"]}{" "}
+        {units === "imperial" ? "mph" : units === "metric" ? "km/h" : "km/h"}
       </h1>
     </div>
   );
