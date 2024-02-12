400005cf4dcb3b4b2c30ba1715f1767642ee0a18
Chris Wood <wchristopherwood912@gmail.com>
2024-02-12 15:04:08 -0800

    fix error checking

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 function Weather(props) {
-  const [weather, setWeather] = useState({ main: { temp: "loading..." } });
+  const [weather, setWeather] = useState({});
   const { city, units } = props;
   useEffect(() => {
     fetch(
@@ -18,21 +18,29 @@ function Weather(props) {
       });
   }, [city, units]);
 
-  if (weather === "error" || weather["main"] === undefined) {
-    return <h1>There was an error when trying to get the weather</h1>;
+  try {
+    const temp = weather["main"]["temp"];
+    const wind = weather["wind"]["speed"];
+    return (
+      <div className="Weather">
+        <h1>
+          The temperature in {city} is {temp}{" "}
+          {units === "imperial" ? "f" : units === "metric" ? "c" : "k"} and the
+          wind speed is {wind}{" "}
+          {units === "imperial" ? "mph" : units === "metric" ? "km/h" : "km/h"}
+        </h1>
+      </div>
+    );
+  } catch (err) {
+    console.log(err);
+    return (
+      <div className="Weather">
+        <h1>
+          <p>error</p>
+        </h1>
+      </div>
+    );
   }
-  console.log(weather);
-
-  return (
-    <div className="Weather">
-      <h1>
-        The temperature in {city} is {weather["main"]["temp"]}{" "}
-        {units === "imperial" ? "f" : units === "metric" ? "c" : "k"} and the
-        wind speed is {weather["wind"]["speed"]}{" "}
-        {units === "imperial" ? "mph" : units === "metric" ? "km/h" : "km/h"}
-      </h1>
-    </div>
-  );
 }
 
 export default Weather;
